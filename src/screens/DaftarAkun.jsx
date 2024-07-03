04599ace1eec7ab53d3f5271b9da2d67efeea504
Yayukk <yayukoktafiana27@gmail.com>
2024-07-03 21:30:43 +0700

    Menambahkan beberapa folder

---
+++ src/screens/DaftarAkun.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const DaftarAkun = () => {
+  return (
+    <View>
+      <Text>DaftarAkun</Text>
+    </View>
+  );
+};
+
+export default DaftarAkun;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Masuk.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Masuk = () => {
+  return (
+    <View>
+      <Text>Masuk</Text>
+    </View>
+  );
+};
+
+export default Masuk;
+
+const styles = StyleSheet.create({});
