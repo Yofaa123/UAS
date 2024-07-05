20098232ab394cc52304d4daa7959219cf7d9683
Yayukk <yayukoktafiana27@gmail.com>
2024-07-06 00:14:13 +0700

    Menambahkan file Favorit, Pesanan dan Pengaturan

---
+++ src/screens/Favorit.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Favorit = () => {
+  return (
+    <View>
+      <Text>Favorit</Text>
+    </View>
+  );
+};
+
+export default Favorit;
+
+const styles = StyleSheet.create({});
